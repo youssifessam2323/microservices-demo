54614bf3be7b924c3a98a106a983279666d68833
youssif essam <youssifessambarky@gmail.com>
2024-11-02 16:36:07 +0200

    - removing the parent interface - feign client don't need to be annotated with stereotypes (@Component, etc) - removing the callback and the qualifier attr as they are not needed

--- order-service/src/main/java/io/joework/orders/client/PaymentFeignClient.java
+++ order-service/src/main/java/io/joework/orders/client/PaymentFeignClient.java
@@ -4,17 +4,14 @@ import io.joework.orders.client.dto.PaymentServiceRequest;
 import io.joework.orders.client.dto.PaymentServiceResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "payments", qualifiers = "paymentFeignClient", fallback = PaymentFeignClientFallback.class)
-@Component
-public interface PaymentFeignClient extends PaymentClient {
+@FeignClient(value = "payments")
+public interface PaymentFeignClient{
 
     String CONTENT_TYPE = "application/json";
 
-    @Override
     @PostMapping(value = "/payments", consumes = {CONTENT_TYPE})
     ResponseEntity<PaymentServiceResponse> processPayments(@RequestBody PaymentServiceRequest request);
 
